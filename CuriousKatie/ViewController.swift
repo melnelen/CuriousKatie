720fd8a64ba67fbeed7e17a5ffbd8f5633eaa6dc
melnelen <melnelen@gmail.com>
2020-04-15 17:41:49 +0200

    Initial Commit

---
+++ CuriousKatie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CuriousKatie
+//
+//  Created by Alexandra Ivanova on 15/04/2020.
+//  Copyright © 2020 Alex Ivanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
